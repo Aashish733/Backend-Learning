22a7eef2fa166261a9c46151d53a890f90b01c55
Aashish909 <aashish24062000@gmail.com>
2026-05-09 19:22:00 +0530

    Add cookie-parser dependency and implement ApiError, ApiResponse, and asyncHandler utilities

---
+++ src/utils/ApiError.js
@@ -0,0 +1,29 @@
+class ApiError extends Error {
+  constructor(
+    statusCode,
+    message= "ApiError went wrong",
+    errors= [],
+    statck=''
+  ){
+
+    //override constructor
+    super(message)
+    this.statusCode = statusCode
+    this.data= null
+    this.message = message
+    this.success = false
+    this.errors = errors
+
+    if(statck){
+      this.stack = statck
+    } else{
+      Error.captureStackTrace(this, this.constructor)
+    }
+
+  }
+}
+
+export {ApiError
+
+
+}
